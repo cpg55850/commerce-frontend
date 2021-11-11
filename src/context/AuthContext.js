3704229a225be5949310b01b94f6c1c59fa257d5
cpg55850 <cpg55850@ucmo.edu>
2021-11-11 14:44:08 -0600

    new comments

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -1,54 +1,54 @@
 import React, { useState, useEffect } from 'react'
 
 const AuthContext = React.createContext({
-	user: '',
-	login: () => {},
-	logout: () => {},
-	loading: true,
+  user: '',
+  login: () => {},
+  logout: () => {},
+  loading: true,
 })
 
 export const AuthContextProvider = ({ children }) => {
-	const [user, setUser] = useState('')
-	const [loading, setLoading] = useState(true)
+  const [user, setUser] = useState('')
+  const [loading, setLoading] = useState(true)
 
-	useEffect(() => {
-		// If the username is stored in a session, set the user to the stored one immediately
-		if (sessionStorage.authenticatedUser) {
-			console.log('User found in sessionStorage, setting the user...')
-			setUser(sessionStorage.authenticatedUser)
-		}
-		setLoading(false)
-	}, [])
+  useEffect(() => {
+    // If the username is stored in a session, set the user to the stored one immediately
+    if (sessionStorage.authenticatedUser) {
+      console.log('User found in sessionStorage, setting the user...')
+      setUser(sessionStorage.authenticatedUser)
+    }
+    setLoading(false)
+  }, [])
 
-	const login = (username, pwd) => {
-		// Need to check if user exists in backend
+  const login = (username, pwd) => {
+    // Need to check if user exists in backend
 
-		// BACKEND TODO
-		// HTTP /GET /users
-		// body: { username }
+    // BACKEND TODO
+    // HTTP /POST /users/auth
+    // body: { email, password }
 
-		// if exists, login
-		// else show error
+    // if exists, login
+    // else show error
 
-		console.log('Logging in... ', username, pwd)
-		sessionStorage.setItem('authenticatedUser', username)
-		setUser(username)
-		setLoading(false)
-	}
+    console.log('Logging in... ', username, pwd)
+    sessionStorage.setItem('authenticatedUser', username)
+    setUser(username)
+    setLoading(false)
+  }
 
-	const logout = () => {
-		sessionStorage.removeItem('authenticatedUser')
-		setUser('')
-		setLoading(false)
-	}
+  const logout = () => {
+    sessionStorage.removeItem('authenticatedUser')
+    setUser('')
+    setLoading(false)
+  }
 
-	const isLoggedIn = user !== '' ? true : false
+  const isLoggedIn = user !== '' ? true : false
 
-	return (
-		<AuthContext.Provider value={{ user, login, logout, isLoggedIn, loading }}>
-			{children}
-		</AuthContext.Provider>
-	)
+  return (
+    <AuthContext.Provider value={{ user, login, logout, isLoggedIn, loading }}>
+      {children}
+    </AuthContext.Provider>
+  )
 }
 
 export const useAuth = () => React.useContext(AuthContext)
--- src/context/ReservationContext.js
+++ src/context/ReservationContext.js
@@ -5,96 +5,100 @@ import { useAlert } from './AlertContext'
 import { useAuth } from './AuthContext'
 
 const ReservationContext = React.createContext([
-	{
-		id: '',
-		name: '',
-		reservations: [],
-	},
+  {
+    id: '',
+    name: '',
+    reservations: [],
+  },
 ])
 
 export const ReservationContextProvider = ({ children }) => {
-	const [reservation, setReservation] = useState([])
-	const { showAlert } = useAlert()
-	const { user } = useAuth()
+  const [reservation, setReservation] = useState([])
+  const { showAlert } = useAlert()
+  const { user } = useAuth()
 
-	// Need to retrieve the currently logged in user's reservations from the backend.
+  // Need to retrieve the currently logged in user's reservations from the backend.
 
-	// BACKEND TODO
-	// HTTP GET /reservations
-	// request body: { userID }
-	// Set the reservations to returned reservations
+  // BACKEND TODO
+  // HTTP GET /users/:id/reservations
+  // Get a list of reservations by the logged in user
+  // Set the reservations to returned reservations
 
-	// TEMP SOLUTION
-	useEffect(() => {
-		let myCubicles = []
+  // TEMP SOLUTION
+  useEffect(() => {
+    let myCubicles = []
 
-		cubicleData.forEach((c) => {
-			console.log('c', c)
-			let isMatch = false
-			c.reservations.forEach((r) => {
-				console.log('r', r)
-				console.log('r.userId', r.userId)
-				console.log('user', user)
-				if (r.userId === user) {
-					isMatch = true
-				}
-			})
+    cubicleData.forEach((c) => {
+      console.log('c', c)
+      let isMatch = false
+      c.reservations.forEach((r) => {
+        console.log('r', r)
+        console.log('r.userId', r.userId)
+        console.log('user', user)
+        if (r.userId === user) {
+          isMatch = true
+        }
+      })
 
-			if (isMatch) {
-				myCubicles.push(c)
-			}
-		})
+      if (isMatch) {
+        myCubicles.push(c)
+      }
+    })
 
-		console.log('setting reservations', myCubicles)
-		setReservation(myCubicles)
-	}, [user])
+    console.log('setting reservations', myCubicles)
+    setReservation(myCubicles)
+  }, [user])
 
-	const addReservation = (cubicle, startTime, endTime) => {
-		// Check if cubicle is already reserved
-		if (reservation.find((o) => o.id === cubicle.id)) {
-			return showAlert(`Cubicle ${cubicle.id} already reserved.`, 'error')
-		}
+  const addReservation = (cubicle, startTime, endTime) => {
+    // BACKEND TODO
+    // HTTP POST /reservations
+    // request body: { userID, cubicleID, startDate, endDate }
 
-		// TEMP - Edit the cubicle to include the new reservation
-		const newCubicle = {
-			...cubicle,
-			reservations: [
-				...cubicle.reservations,
-				{
-					userId: user,
-					startTime: moment(startTime).format('YYYY-MM-DD'),
-					endTime: moment(endTime).format('YYYY-MM-DD'),
-				},
-			],
-		}
+    // Check if cubicle is already reserved
+    if (reservation.find((o) => o.id === cubicle.id)) {
+      return showAlert(`Cubicle ${cubicle.id} already reserved.`, 'error')
+    }
 
-		console.log(user)
-		console.log(cubicle)
-		console.log('new cubicle', newCubicle)
+    // TEMP - Edit the cubicle to include the new reservation
+    const newCubicle = {
+      ...cubicle,
+      reservations: [
+        ...cubicle.reservations,
+        {
+          userId: user,
+          startTime: moment(startTime).format('YYYY-MM-DD'),
+          endTime: moment(endTime).format('YYYY-MM-DD'),
+        },
+      ],
+    }
 
-		// BACKEND TODO
-		// HTTP POST /reservations
-		// request body: { userID, cubicleID, startTime, endTime }
-		setReservation([...reservation, newCubicle])
-		showAlert(`Reservation ${cubicle.id} added!`)
-	}
+    console.log(user)
+    console.log(cubicle)
+    console.log('new cubicle', newCubicle)
 
-	const removeReservation = (cubicle) => {
-		// Create a new array of cubicles with the passed in cubicle removed
-		const filtered = reservation.filter((current) => current.id !== cubicle.id)
+    setReservation([...reservation, newCubicle])
+    showAlert(`Reservation ${cubicle.id} added!`)
+  }
 
-		// Set the reservations to that new array and alert the user
-		showAlert(`Reservation ${cubicle.id} deleted!`)
-		setReservation([...filtered])
-	}
+  const removeReservation = (cubicle) => {
+    // BACKEND TODO
+    // HTTP DELETE /reservations/:id
 
-	return (
-		<ReservationContext.Provider
-			value={{ reservation, addReservation, removeReservation }}
-		>
-			{children}
-		</ReservationContext.Provider>
-	)
+    // Create a new array of cubicles with the passed in cubicle removed
+    const filtered = reservation.filter((current) => current.id !== cubicle.id)
+
+    // Set the reservations to that new array and alert the user
+    showAlert(`Reservation ${cubicle.id} deleted!`)
+    setReservation([...filtered])
+  }
+
+  return (
+    <ReservationContext.Provider
+      value={{ reservation, addReservation, removeReservation }}
+    >
+      {children}
+    </ReservationContext.Provider>
+  )
 }
 
 export const useReservation = () => React.useContext(ReservationContext)
